c89f8b1f00c4a57e4d18434a7ced5d6a1399b68e
afsakmu <142154597+afsakmu@users.noreply.github.com>
2024-12-05 13:37:22 +0700

    commit-perhitungan-js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
 // File JS Script
+
+// pengumpulan data lewat id
+function validatefirstform() {
+    let inputBeratBadan = document.getElementById('berat-badan-input').value
+    let inputUsia = document.getElementById('usia-input').value
+    let inputTinggiBadan = document.getElementById('tinggi-badan-input').value
+
+    // pengaturan kalkulator BMI
+    console.log('BB:' + inputBeratBadan)
+    console.log('Usia:' + inputUsia)
+    console.log('Tinggi:' + inputTinggiBadan)
+    
+    // perhitungan kalkulator BMI
+    if (inputBeratBadan != '' && inputUsia != '' && inputTinggiBadan != '') {
+        let beratBadan = parseFloat(inputBeratBadan)
+        let tinggibadan = parseFloat(inputTinggiBadan)
+        let calculate = beratBadan / (tinggibadan ** 2)
+        updateresult(calculate.toFixed(2));
+    } else {
+        alert('Tidak bisa diproses, Mohon lengkapi data anda !'); //notifikasi bila data tidak lengkap
+    }
+}
+
+// tampilan hasil perhitungan
+function updateresult(value) {
+    document.getElementById('result-calculation').innerHTML = value;
+}
